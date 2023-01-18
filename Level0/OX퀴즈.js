79e758f54e1ea8c5ae7217717e4650e9f3c5e7c8
Ryum <ryumforart@gmail.com>
2023-01-19 00:37:04 +0900

    Solve: OX퀴즈, 디렉토리 수정

---
+++ Level0/OX퀴즈.js
@@ -0,0 +1,28 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120907
+
+function solution(quiz) {
+
+    let answer = [];
+    
+    for (let i = 0; i < quiz.length; i++) {
+        let firstTerm = Number(quiz[i].split(' ')[0]);
+        let secondTerm = Number(quiz[i].split(' ')[2]);
+        let arithmatic = quiz[i].split(' ')[1];
+        let result = quiz[i].split(' ')[4];
+        
+        if (arithmatic == '+') {
+            if (result == (firstTerm + secondTerm)) {
+                answer.push('O');
+            } else {
+                answer.push('X');
+            }
+        } else if (arithmatic == '-') {
+            if (result == (firstTerm - secondTerm)) {
+                answer.push('O');
+            } else {
+                answer.push('X');
+            }
+        }
+    }
+    return answer;
+}
